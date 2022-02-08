a9e2bd49570cd345632afeb4d95feba2daec1704
Aditya7a <aditya.prakash.18csc@bmu.edu.in>
2022-02-09 00:45:40 +0530

    commit-1

--- components/Search.js
+++ components/Search.js
@@ -1,17 +1,43 @@
+import React, { useState } from "react";
+
 function Search() {
+    const types = ["3-wheelers", "2-wheelers", "Bicycles", "Cars"];
+    const active = "opacity-1";
+
+    const [style, setStyle] = useState(types[0]);
+
+    const changeStyle = () => {
+        console.log("you just clicked");
+
+        setStyle(console.log({ types }));
+    };
     return (
         <>
             <div className='my_grid mt-2'>
                 <section className='col-start-2 flex flex-col'>
-                    <div className='bg-white_2 text-my_gray2 col-start-2 flex h-16 w-max flex-row rounded-lg text-sm font-semibold '>
-                        <button
-                            type='button'
-                            className='dark:focus:ring-blue-800" hover:bg-b1 bg-white_2 rounded-lg px-5 py-2.5 text-center text-sm hover:text-white '>
-                            3-wheeler
-                        </button>
-                        <button className='m-auto mx-6'>2-wheeler</button>
-                        <button className='m-auto mx-6'>Bicycle</button>
-                        <button className='m-auto mx-6'>Car</button>
+                    <div className='bg-white_2 text-my_gray2 col-start-2 flex h-16 w-max flex-row rounded-lg text-sm font-semibold active:bg-white'>
+                        <div className='focus:ring-blue-800" hover:bg-b1 bg-white_2 m-auto gap-2 rounded-lg px-4 py-2.5 text-center text-sm hover:text-white'>
+                            {types.map((type) => (
+                                <button
+                                    key={type}
+                                    id={type}
+                                    className='px-4 py-2.5'
+                                    onClick={changeStyle}>
+                                    {type}
+                                </button>
+                            ))}
+                            {/* <div>
+                                {types.map((type) => (
+                                    <button
+                                        className='px-4 py-2.5'
+                                        key={type}
+                                        active={active === type}
+                                        onClick={() => setStyle(type)}>
+                                        {type}
+                                    </button>
+                                ))}
+                            </div> */}
+                        </div>
                     </div>
 
                     <div className='mt-2  w-full  sm:mx-auto'>
@@ -29,7 +55,7 @@ function Search() {
                                         </label>
                                         <select
                                             id='countries'
-                                            className='text-my_gray2 focus:ring-b1 focus:border-b1 block w-full rounded-lg border border-white p-2.5 text-base'>
+                                            className='text-my_gray2 focus:ring-b1 focus:border-b1 block w-full rounded-lg border border-white p-2.5 text-base shadow-none'>
                                             <option className='py-2'>
                                                 Tesla
                                             </option>
@@ -47,7 +73,7 @@ function Search() {
                                         </label>
                                         <select
                                             id='countries'
-                                            className='border-my_gray text-my_gray2 focus:ring-b1 focus:border-b1 block w-full rounded-lg border p-2.5 text-base'>
+                                            className='text-my_gray2 focus:ring-b1 focus:border-b1 block w-full rounded-lg border border-white p-2.5 text-base shadow-none'>
                                             <option className='py-2'>
                                                 Tesla
                                             </option>
@@ -65,7 +91,7 @@ function Search() {
                                         </label>
                                         <select
                                             id='countries'
-                                            className='border-my_gray text-my_gray2 focus:ring-b1 focus:border-b1 block w-full rounded-lg border p-2.5 text-base'>
+                                            className='text-my_gray2 focus:ring-b1 focus:border-b1 block w-full rounded-lg border border-white p-2.5 text-base shadow-none'>
                                             <option className='py-2'>
                                                 Tesla
                                             </option>
@@ -83,7 +109,7 @@ function Search() {
                                         </label>
                                         <select
                                             id='countries'
-                                            className='border-my_gray text-my_gray2 focus:ring-b1 focus:border-b1 block w-full rounded-lg border p-2.5 text-base'>
+                                            className='text-my_gray2 focus:ring-b1 focus:border-b1 block w-full rounded-lg border border-white p-2.5 text-base shadow-none'>
                                             <option className='py-2'>
                                                 Tesla
                                             </option>
